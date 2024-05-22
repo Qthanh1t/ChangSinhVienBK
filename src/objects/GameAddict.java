b1119bf580c885238cda77d597895cd814f3d282
Shido Pham <huyson22092004@gmail.com>
2024-05-22 09:41:24 +0700

    fix hitbox

--- src/objects/GameAddict.java
+++ src/objects/GameAddict.java
@@ -6,16 +6,9 @@ public class GameAddict extends GameObject {
 
     public GameAddict(int x, int y, int objType) {
         super(x, y, objType);
-<<<<<<< HEAD
-        initHitbox(22, 14);
-        xDrawOffset = (int) (6 * Game.SCALE);
-        yDrawOffset = (int) (9 * Game.SCALE);
-
-=======
         initHitbox(25, 25);
         xDrawOffset = 0;
         yDrawOffset = 0;
->>>>>>> origin/dev
         hitbox.y += yDrawOffset;
     }
 
