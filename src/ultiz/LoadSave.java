2206c006b683488fd140a548a237318b826e18b8
Shido Pham <huyson22092004@gmail.com>
2024-05-26 09:44:29 +0700

    update file input

--- src/ultiz/LoadSave.java
+++ src/ultiz/LoadSave.java
@@ -1,11 +1,12 @@
 package ultiz;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
+
+import java.util.Arrays;
+import java.util.List;
+
 import javax.imageio.ImageIO;
 
 public class LoadSave {
@@ -52,35 +53,21 @@ public class LoadSave {
 	}
 
 	public static BufferedImage[] GetAllLevels() {
-		URL url = LoadSave.class.getResource("/lvls");
-		File file = null;
-		
-		try {
-			file = new File(url.toURI());
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
+        List<String> levelNames = Arrays.asList("1.png", "2.png", "3.png", "4.png");
+        BufferedImage[] levels = new BufferedImage[levelNames.size()];
 
-		File[] files = file.listFiles();
-		File[] filesSorted = new File[files.length];
-
-		for (int i = 0; i < filesSorted.length; i++) 
-			for (int j = 0; j < files.length; j++) {
-				if (files[j].getName().equals((i + 1) + ".png"))
-					filesSorted[i] = files[j];
-
-			}
-
-		BufferedImage[] imgs = new BufferedImage[filesSorted.length];
-
-		for (int i = 0; i < imgs.length; i++)
-			try {
-				imgs[i] = ImageIO.read(filesSorted[i]);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-		return imgs;
-	}
+        for(int i = 0; i < levels.length; i++) {
+            try (InputStream is = LoadSave.class.getResourceAsStream("/lvls/" + levelNames.get(i))) {
+                if (is == null) {
+                    System.err.println("File not found:\n" + levelNames.get(i));
+                    System.exit(1);
+                }
+                levels[i] = ImageIO.read(is);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return levels;
+    }
 
 }
